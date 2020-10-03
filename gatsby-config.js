47d8b800fbce173a8f3cca31bcef94f3a32de9e8
knada <nanaayimda@gmail.com>
2020-10-03 11:05:30 +0000

    Added breakpoint values, added site icon and personalised package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,13 +22,13 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
+                name: `innospace-website`,
+                short_name: `innospace`,
                 start_url: `/`,
                 background_color: `#663399`,
                 theme_color: `#663399`,
                 display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+                icon: `src/images/innospace-icon.png`, // This path is relative to the root of the site.
             },
         },
         {
