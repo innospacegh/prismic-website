6c0c056a2f71544c8e7c4e879f64e10d133b51d4
knada <nanaayimda@gmail.com>
2020-10-04 20:51:06 +0000

    Added scaffolding for spaces section and space components

---
+++ src/components/Space/Space.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+const SpaceContainer = styled.div``
+
+const Space = () => <SpaceContainer />
+
+export default Space
---
+++ src/components/SpacesSection/SpacesSection.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+const SpacesSectionContainer = styled.section``
+
+const SpacesSection = () => <SpacesSectionContainer />
+
+export default SpacesSection
