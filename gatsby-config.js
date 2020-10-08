b175643eb702d8e30d363ae9ffdc73816ca2162c
knada <nanaayimda@gmail.com>
2020-10-08 00:50:09 +0000

    Added gatsby-plugin-styled-components to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,6 +66,7 @@ module.exports = {
                 lang: '*',
             },
         },
+        `gatsby-plugin-styled-components`,
         `gatsby-plugin-typescript`,
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
